02669851212e16f8492f217cc4f0e972132c31f6
bhoff <bruce.hoff@sagebase.org>
2017-05-18 14:17:35 -0700

    SYNR-1090

--- inst/integrationTests/test_QueryResult.R
+++ inst/integrationTests/test_QueryResult.R
@@ -21,6 +21,8 @@
   deleteEntity(synapseClient:::.getCache("testProject"))
 }
 
+timeToWait<-"00:10:00" # wait for up to ten minutes
+
 integrationTestQueryResult_Fetch <- function() {
   project <- synapseClient:::.getCache("testProject")$properties$id
 
@@ -29,10 +31,11 @@ integrationTestQueryResult_Fetch <- function() {
   lapply(1:10, function(i) createEntity(folder))
 
 	startTime<-Sys.time()
-	while (Sys.time()-startTime<as.difftime("00:05:00")) { # wait for up to five minutes
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
   	qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s'", project), blockSize=5)
   	df <- qr$fetch()
 		if (nrow(df)==5) break
+		message("Expected 5 rows but found ", nrow(df))
 		Sys.sleep(5)
 	}
 	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
@@ -41,9 +44,10 @@ integrationTestQueryResult_Fetch <- function() {
   checkEquals(ncol(df),3)
 	
 	startTime<-Sys.time()
-	while (Sys.time()-startTime<as.difftime("00:05:00")) { # wait for up to five minutes
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
 		df <- qr$fetch()
 		if (nrow(df)==5) break
+		message("Expected 5 rows but found ", nrow(df))
 		Sys.sleep(5)
 	}
 	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
@@ -65,10 +69,11 @@ integrationTestQueryResult_Collect <- function() {
   lapply(1:10, function(i) createEntity(folder))
 
 	startTime<-Sys.time()
-	while (Sys.time()-startTime<as.difftime("00:05:00")) { # wait for up to five minutes
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
 		qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s'", project), blockSize=3)
 		df <- qr$collect()
 		if (nrow(df)==3) break
+		message("Expected 3 rows but found ", nrow(df))
 		Sys.sleep(5)
 	}
 	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
@@ -77,9 +82,10 @@ integrationTestQueryResult_Collect <- function() {
   checkEquals(ncol(df),3)
 	
 	startTime<-Sys.time()
-	while (Sys.time()-startTime<as.difftime("00:05:00")) { # wait for up to five minutes
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
 		df <- qr$collect()
 		if (nrow(df)==3) break
+		message("Expected 3 rows but found ", nrow(df))
 		Sys.sleep(5)
 	}
 	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
@@ -89,9 +95,10 @@ integrationTestQueryResult_Collect <- function() {
 	
 	
 	startTime<-Sys.time()
-	while (Sys.time()-startTime<as.difftime("00:05:00")) { # wait for up to five minutes
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
 		df <- qr$collect()
 		if (nrow(df)==3) break
+		message("Expected 3 rows but found ", nrow(df))
 		Sys.sleep(5)
 	}
 	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
@@ -113,11 +120,12 @@ integrationTestQueryResult_CollectAll <- function() {
 	
 	
 	startTime<-Sys.time()
-	while (Sys.time()-startTime<as.difftime("00:05:00")) { # wait for up to five minutes
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
 		qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s' LIMIT 10", project), blockSize=7)
 		qr$collect()
 		df <- qr$collectAll()
 		if (nrow(df)==10) break
+		message("Expected 10 rows but found ", nrow(df))
 		Sys.sleep(5)
 	}
 	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
